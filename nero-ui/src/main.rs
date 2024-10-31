194f0f8da2cd1fa139b35c937222f8335c060527
dacchidocchi <dacchidocchi@gmail.com>
2024-10-30 21:58:37 -0300

    ui: bump leptos to 0.7.0-rc0

--- nero-ui/src/main.rs
+++ nero-ui/src/main.rs
@@ -1,4 +1,8 @@
-use leptos::{mount_to_body, view};
+use leptos::{
+    html::{h1, ElementChild},
+    mount::mount_to_body,
+    prelude::ClassAttribute,
+};
 use typewind::{
     customization::{Color, ColorTone},
     typography::TextColor,
@@ -8,9 +12,5 @@ fn main() {
     console_error_panic_hook::set_once();
 
     let color = TextColor(Color::Red(ColorTone::_500)).to_string();
-    mount_to_body(|| {
-        view! {
-            <h1 class={color}>"Hello World"</h1>
-        }
-    })
+    mount_to_body(|| h1().class(color).child("Hello, world!"))
 }
