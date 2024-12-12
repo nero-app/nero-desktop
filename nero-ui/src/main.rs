a65377ef2aec3956abf90760d7ce3133e6cc0d4c
dacchidocchi <dacchidocchi@gmail.com>
2024-12-12 19:25:53 -0300

    feat: move typewind to its own repository

--- nero-ui/src/main.rs
+++ nero-ui/src/main.rs
@@ -4,16 +4,13 @@ use sycamore::{
     render,
     web::{tags::h1, GlobalProps, HtmlGlobalAttributes},
 };
-use typewind::{
-    customization::{Color, ColorTone},
-    typography::{FontFamily, FontSize, TextColor},
-};
+use typewind::typography::{FontFamily, FontSize, TextColor};
 
 fn main() {
     console_error_panic_hook::set_once();
 
     let title = h1().children("Hello, World!").class(format2!(
-        TextColor(Color::Red(ColorTone::_500)),
+        TextColor::Red500,
         FontFamily::Mono,
         FontSize::_3xl,
     ));
