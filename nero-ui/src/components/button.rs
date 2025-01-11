080f8a73a6e53428881443b41e32af10e159fe4c
dacchidocchi <dacchidocchi@gmail.com>
2025-01-10 21:43:35 -0300

    ui: add `box_shadow` property to `Button` component

--- nero-ui/src/components/button.rs
+++ nero-ui/src/components/button.rs
@@ -2,6 +2,7 @@ use rustwind::{
     active,
     backgrounds::BackgroundColor,
     borders::BorderRadius,
+    effects::BoxShadow,
     flexbox_grid::{AlignItems, Gap},
     layout::Display,
     spacing::Padding,
@@ -24,6 +25,7 @@ where
 {
     children: View,
     color: Option<BackgroundColor>,
+    box_shadow: Option<BoxShadow>,
     on_click: T,
 }
 
@@ -35,12 +37,18 @@ where
         Self {
             children: children.into(),
             color: None,
+            box_shadow: None,
             on_click,
         }
     }
 
     pub fn color(mut self, color: BackgroundColor) -> Self {
         self.color = Some(color);
+        self
+    }
+
+    pub fn box_shadow(mut self, box_shadow: BoxShadow) -> Self {
+        self.box_shadow = Some(box_shadow);
         self
     }
 
@@ -60,6 +68,7 @@ where
                 .children(span().children(label)),
             on_click,
         )
+        .box_shadow(BoxShadow::Lg)
     }
 }
 
@@ -67,7 +76,7 @@ impl<T: FnMut(MouseEvent)> From<Button<T>> for View {
     fn from(button: Button<T>) -> Self {
         button_tag()
             .class(format!(
-                "{} {}",
+                "{} {} {}",
                 tw!(
                     Padding::Px3,
                     Padding::Py1_5,
@@ -78,7 +87,8 @@ impl<T: FnMut(MouseEvent)> From<Button<T>> for View {
                 button
                     .color
                     .unwrap_or(BackgroundColor::Transparent)
-                    .as_class()
+                    .as_class(),
+                button.box_shadow.unwrap_or(BoxShadow::None).as_class()
             ))
             .children(button.children)
             .on(click, button.on_click)
