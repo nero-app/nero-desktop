1f52102fdeac4ea46ea8ae6aafbe1ede027ce40e
dacchidocchi <dacchidocchi@gmail.com>
2025-03-25 21:57:23 -0300

    ui: move `invoke_and_parse` function to utils

--- nero-ui/src/extensions.rs
+++ nero-ui/src/extensions.rs
@@ -1,39 +1,16 @@
 use nero_extensions::{
-    anyhow::{anyhow, Result},
+    anyhow::Result,
     types::{EpisodesPage, FilterCategory, SearchFilter, Series, SeriesPage, SeriesVideo},
     Extension as ExtensionTrait,
 };
-use serde_wasm_bindgen::{from_value, to_value};
-use wasm_bindgen::UnwrapThrowExt;
 
-use crate::{invoke, invoke_without_args};
+use crate::utils::invoke_and_parse;
 
 pub struct Extension;
 
-impl Extension {
-    async fn invoke_and_parse<T>(cmd: &str, args: Option<serde_json::Value>) -> Result<T>
-    where
-        T: serde::de::DeserializeOwned,
-    {
-        let res = match args {
-            Some(args_value) => {
-                let js_value = to_value(&args_value).unwrap_throw();
-                invoke(cmd, js_value)
-                    .await
-                    .map_err(|e| anyhow!("Error invoking {}: {:?}", cmd, e))?
-            }
-            None => invoke_without_args(cmd)
-                .await
-                .map_err(|e| anyhow!("Error invoking {}: {:?}", cmd, e))?,
-        };
-
-        from_value::<T>(res).map_err(|e| anyhow!("Error parsing result from {}: {:?}", cmd, e))
-    }
-}
-
 impl ExtensionTrait for Extension {
     async fn filters(&self) -> Result<Vec<FilterCategory>> {
-        Self::invoke_and_parse("get_filters", None).await
+        invoke_and_parse("get_filters", None).await
     }
 
     async fn search(
@@ -43,12 +20,12 @@ impl ExtensionTrait for Extension {
         filters: Vec<SearchFilter>,
     ) -> Result<SeriesPage> {
         let args = serde_json::json!({ "query": query, "page": page, "filters": filters });
-        Self::invoke_and_parse("search", Some(args)).await
+        invoke_and_parse("search", Some(args)).await
     }
 
     async fn get_series_info(&self, series_id: &str) -> Result<Series> {
         let args = serde_json::json!({ "series_id": series_id });
-        Self::invoke_and_parse("get_series_info", Some(args)).await
+        invoke_and_parse("get_series_info", Some(args)).await
     }
 
     async fn get_series_episodes(
@@ -57,7 +34,7 @@ impl ExtensionTrait for Extension {
         page: Option<u16>,
     ) -> Result<EpisodesPage> {
         let args = serde_json::json!({ "series_id": series_id, "page": page });
-        Self::invoke_and_parse("get_series_episodes", Some(args)).await
+        invoke_and_parse("get_series_episodes", Some(args)).await
     }
 
     async fn get_series_videos(
@@ -66,6 +43,6 @@ impl ExtensionTrait for Extension {
         episode_id: &str,
     ) -> Result<Vec<SeriesVideo>> {
         let args = serde_json::json!({ "series_id": series_id, "episode_id": episode_id });
-        Self::invoke_and_parse("get_series_videos", Some(args)).await
+        invoke_and_parse("get_series_videos", Some(args)).await
     }
 }
--- nero-ui/src/utils.rs
+++ nero-ui/src/utils.rs
@@ -1,6 +1,10 @@
+use nero_extensions::anyhow::{anyhow, Result};
+use serde_wasm_bindgen::{from_value, to_value};
 use sycamore::web::{window, View};
 use sycamore_router::navigate_no_history;
 use wasm_bindgen::{JsValue, UnwrapThrowExt};
+
+use crate::{invoke, invoke_without_args};
 
 pub trait ViewBuilder: Sized {
     fn map<U>(self, f: impl FnOnce(Self) -> U) -> U {
@@ -30,4 +34,23 @@ pub fn navigate_with_state(url: &str, data: &JsValue) {
         .push_state_with_url(data, "", Some(url))
         .unwrap_throw();
     navigate_no_history(url);
+}
+
+pub async fn invoke_and_parse<T>(cmd: &str, args: Option<serde_json::Value>) -> Result<T>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let res = match args {
+        Some(args_value) => {
+            let js_value = to_value(&args_value).unwrap_throw();
+            invoke(cmd, js_value)
+                .await
+                .map_err(|e| anyhow!("Error invoking {}: {:?}", cmd, e))?
+        }
+        None => invoke_without_args(cmd)
+            .await
+            .map_err(|e| anyhow!("Error invoking {}: {:?}", cmd, e))?,
+    };
+
+    from_value::<T>(res).map_err(|e| anyhow!("Error parsing result from {}: {:?}", cmd, e))
 }
