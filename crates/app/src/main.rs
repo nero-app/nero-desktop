f2f0febe96ce477e9895d3b5ae20237b2b1639b5
dacchidocchi <dacchidocchi@gmail.com>
2025-11-17 02:14:40 -0300

    app: setup tauri-plugin-dialog

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let processor_addr = SocketAddr::from(([127, 0, 0, 1], processor_port));
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_nero_extensions::Builder::new(processor_addr).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
