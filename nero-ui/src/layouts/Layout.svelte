a5a221ce482e43ca4b53b993678776b36f66d725
dacchidocchi <dacchidocchi@gmail.com>
2026-02-25 02:24:03 -0300

    feat: remove padding and toolbar from base layout

--- nero-ui/src/layouts/Layout.svelte
+++ nero-ui/src/layouts/Layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import Toolbar from "../components/Toolbar.svelte";
   import type { Snippet } from "svelte";
 
   let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="fixed flex h-screen w-full flex-col gap-4 px-12 pt-4">
-  <Toolbar />
+<div class="fixed flex h-screen w-full flex-col gap-4">
   <main class="h-full overflow-auto">
     {@render children?.()}
   </main>
