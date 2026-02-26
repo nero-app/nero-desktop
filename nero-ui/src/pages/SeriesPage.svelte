b9a244555e3bbb37c4f3efc60e31ab3c3b01ad4c
dacchidocchi <dacchidocchi@gmail.com>
2026-02-26 16:35:38 -0300

    chore: change background color to white

--- nero-ui/src/pages/SeriesPage.svelte
+++ nero-ui/src/pages/SeriesPage.svelte
@@ -25,8 +25,8 @@
     // if (!extension) throw new Error("No extension loaded");
     // const result = await extension.getSeriesEpisodes(params.seriesId, page);
     // return {
-    // data: result.items,
-    // hasNextPage: result.hasNextPage,
+    //   data: result.items,
+    //   hasNextPage: result.hasNextPage,
     // };
     return {
       data: [],
@@ -50,12 +50,12 @@
   }
 </script>
 
-<div class="grid h-full grid-cols-[2fr_3fr]">
+<div class="grid h-full grid-cols-[2fr_3fr] bg-white">
   <figure class="overflow-hidden">
     {#if seriesQuery.isLoading}
-      <p>Loading...</p>
+      <p class="text-neutral-600">Loading...</p>
     {:else if seriesQuery.isError}
-      <div class="size-full rounded-xl bg-gray-700"></div>
+      <div class="size-full rounded-xl bg-gray-200"></div>
     {:else if seriesQuery.data}
       <img
         class="size-full object-cover"
@@ -65,33 +65,36 @@
     {/if}
   </figure>
 
-  <article class="flex flex-col justify-center gap-4 overflow-y-auto p-8">
+  <article
+    class="flex flex-col justify-center gap-4 overflow-y-auto bg-white px-8
+      pt-4"
+  >
     {#if seriesQuery.isLoading}
-      <p>Loading...</p>
+      <p class="text-neutral-600">Loading...</p>
     {:else if seriesQuery.error}
       <ErrorMessage
         message="Apparently an error has occurred"
         error={seriesQuery.error}
       />
     {:else if seriesQuery.data}
-      <header class="flex flex-col gap-4 pt-12">
-        <h1 class="truncate text-4xl font-bold text-neutral-100">
+      <header class="flex flex-col gap-4">
+        <h1 class="truncate text-4xl font-bold text-neutral-900">
           {seriesQuery.data.title}
         </h1>
-        <div class="flex items-center gap-1.5 text-sm text-neutral-300">
+        <div class="flex items-center gap-1.5 text-sm text-neutral-500">
           <span>{seriesQuery.data.type}</span>
           <span>•</span>
           <!-- TODO: -->
           <span>some-extension@v0.1.0-draft</span>
         </div>
-        <p class="line-clamp-3 text-sm leading-relaxed text-neutral-300">
+        <p class="line-clamp-3 text-sm leading-relaxed text-neutral-600">
           {seriesQuery.data.synopsis}
         </p>
         <div class="flex w-full items-center gap-6 py-4">
           <button
             class="flex flex-1 cursor-pointer items-center justify-center gap-2.5
-              rounded-lg bg-neutral-100 px-12 py-2 text-sm font-semibold
-              text-neutral-900 transition-colors hover:bg-black/5"
+              rounded-lg bg-neutral-800 px-12 py-2 text-sm
+              text-neutral-100 transition-colors hover:bg-neutral-800"
             onclick={openFirstEpisode}
           >
             <PlayIcon size={24} fill="currentColor" strokeWidth={0} />
@@ -99,19 +102,19 @@
           </button>
           <button
             class="flex size-10 shrink-0 cursor-pointer items-center justify-center
-              rounded-full border-[1.5px] border-neutral-500 text-neutral-100
-              transition-colors hover:border-neutral-600 hover:bg-black/5"
+              rounded-full border-[1.5px] border-neutral-300 text-neutral-700
+              transition-colors hover:border-neutral-400 hover:bg-neutral-100"
             title="Share"
           >
-            <Share2Icon size={15} />
+            <Share2Icon size={20} />
           </button>
           <button
             class="flex size-10 shrink-0 cursor-pointer items-center justify-center
-              rounded-full border-[1.5px] border-neutral-500 text-neutral-100
-              transition-colors hover:border-neutral-600 hover:bg-black/5"
+              rounded-full border-[1.5px] border-neutral-300 text-neutral-700
+              transition-colors hover:border-neutral-400 hover:bg-neutral-100"
             title="Like"
           >
-            <ThumbsUpIcon size={15} strokeWidth={2.5} />
+            <ThumbsUpIcon size={20} strokeWidth={2} />
           </button>
         </div>
       </header>
@@ -119,27 +122,27 @@
 
     <Tabs.Root value="episodes">
       <Tabs.List
-        class="flex w-full justify-between border-t border-neutral-600"
+        class="flex w-full justify-between border-t border-neutral-200"
       >
         <Tabs.Trigger
           value="episodes"
           class="tab-trigger flex-1 cursor-pointer py-2.5 text-center text-sm
-            text-neutral-400 transition-colors hover:text-neutral-300
+            text-neutral-500 transition-colors hover:text-neutral-700
             data-[state=active]:border-t-2
-            data-[state=active]:border-neutral-100
+            data-[state=active]:border-neutral-900
             data-[state=active]:font-semibold
-            data-[state=active]:text-neutral-100"
+            data-[state=active]:text-neutral-900"
         >
           Episodes
         </Tabs.Trigger>
         <Tabs.Trigger
           value="more"
           class="tab-trigger flex-1 cursor-pointer py-2.5 text-center text-sm
-            text-neutral-400 transition-colors hover:text-neutral-300
+            text-neutral-500 transition-colors hover:text-neutral-700
             data-[state=active]:border-t-2
-            data-[state=active]:border-neutral-100
+            data-[state=active]:border-neutral-900
             data-[state=active]:font-semibold
-            data-[state=active]:text-neutral-100"
+            data-[state=active]:text-neutral-900"
         >
           More like this
         </Tabs.Trigger>
@@ -147,7 +150,7 @@
 
       <Tabs.Content value="episodes" class="pt-2">
         {#if episodesQuery.isLoading}
-          <p class="text-center text-sm text-neutral-400">Loading...</p>
+          <p class="text-center text-sm text-neutral-500">Loading...</p>
         {:else if episodesQuery.error}
           <ErrorMessage
             message="Apparently an error has occurred"
@@ -165,14 +168,14 @@
           </div>
           <div bind:this={infiniteScroll.element}></div>
           {#if episodesQuery.isFetchingNextPage}
-            <p class="p-2 text-center text-sm text-neutral-400">
+            <p class="p-2 text-center text-sm text-neutral-500">
               Loading more...
             </p>
           {/if}
         {/if}
       </Tabs.Content>
 
-      <Tabs.Content value="more" class="text-neutral-400">
+      <Tabs.Content value="more" class="text-neutral-500">
         Here goes the more like this
       </Tabs.Content>
     </Tabs.Root>
@@ -200,6 +203,6 @@
     transform: translateX(-50%);
     border-left: 6px solid transparent;
     border-right: 6px solid transparent;
-    border-top: 6px solid white;
+    border-top: 6px solid black;
   }
 </style>
